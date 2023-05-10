18ed842d8b433399281b8cfdeec5accdb60b1115
Chamomile <tima.chamomile@gmail.com>
2023-05-10 13:42:10 +0300

    Add highlightWin method

--- main.js
+++ main.js
@@ -60,13 +60,12 @@ const game = {
     update() {
         this.updateMarks();
         this.switchPlayer();
-        if (this.gameIsOver()) this.enableRestartButton();
+        this.endGame(this.gameIsOver());
     },
 
     setMark(e) {
         const id = e.target.id.slice(-1);
         game.board[id] = game.currentPlayer.id;
-        console.log(game.board);
 
         game.disableCell(id);
         game.update();
@@ -102,21 +101,21 @@ const game = {
 
     winExists() {
         // Horizontal wins
-        if (this.board.slice(0, 3).every((item, _, arr) => item === arr[0] && item !== 0)) return this.board[0];
-        if (this.board.slice(3, 6).every((item, _, arr) => item === arr[0] && item !== 0)) return this.board[3];
-        if (this.board.slice(6, 9).every((item, _, arr) => item === arr[0] && item !== 0)) return this.board[6];
+        if (this.board.slice(0, 3).every((item, _, arr) => item === arr[0] && item !== 0)) return [0, 1, 2];
+        if (this.board.slice(3, 6).every((item, _, arr) => item === arr[0] && item !== 0)) return [3, 4, 5];
+        if (this.board.slice(6, 9).every((item, _, arr) => item === arr[0] && item !== 0)) return [6, 7, 8];
 
         // Vertical wins
-        if (this.board.filter((_, index) => index % 3 === 0).every((item, _, arr) => item === arr[0] && item !== 0)) return this.board[0];
-        if (this.board.filter((_, index) => (index - 1) % 3 === 0).every((item, _, arr) => item === arr[0] && item !== 0)) return this.board[1];
-        if (this.board.filter((_, index) => (index - 2) % 3 === 0).every((item, _, arr) => item === arr[0] && item !== 0)) return this.board[2];
+        if (this.board.filter((_, index) => index % 3 === 0).every((item, _, arr) => item === arr[0] && item !== 0)) return [0, 3, 6];
+        if (this.board.filter((_, index) => (index - 1) % 3 === 0).every((item, _, arr) => item === arr[0] && item !== 0)) return [1, 4, 7];
+        if (this.board.filter((_, index) => (index - 2) % 3 === 0).every((item, _, arr) => item === arr[0] && item !== 0)) return [2, 5, 8];
 
         // Diagonal wins
-        if (this.board.filter((_, index) => index % 4 === 0).every((item, _, arr) => item === arr[0] && item !== 0)) return this.board[0];
-        if (this.board[2] === this.board[4] && this.board[2] === this.board[6] && this.board[2] !== 0) return this.board[2];
+        if (this.board.filter((_, index) => index % 4 === 0).every((item, _, arr) => item === arr[0] && item !== 0)) return [0, 4, 8];
+        if (this.board[2] === this.board[4] && this.board[2] === this.board[6] && this.board[2] !== 0) return [2, 4, 6];
 
         // No win
-        return 0;
+        return false;
     },
 
     boardIsFull() {
@@ -135,6 +134,19 @@ const game = {
 
     disableRestartButton() {
         document.querySelector('#restart-button').disabled = true;
+    },
+
+    endGame(result) {
+        if (!result) return;
+        
+        this.highlightWin(result);
+        this.enableRestartButton();
+    },
+
+    highlightWin(arr) {
+        for (const cell of arr) {
+            document.querySelector(`#cell-${cell}`).classList.add('winning');
+        }
     }
 }
 
